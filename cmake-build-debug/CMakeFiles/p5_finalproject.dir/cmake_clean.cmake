56e5a712f7e905da1e907daeb5b81e4c9bb47253
Min Jae Lee <minjl5@berkeley.edu>
2019-04-30 18:17:50 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/p5_finalproject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5_finalproject.dir/main.cpp.o"
+  "p5_finalproject.pdb"
+  "p5_finalproject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5_finalproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
