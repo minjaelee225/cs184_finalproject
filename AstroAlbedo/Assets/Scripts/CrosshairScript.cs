34af57b983b66683550fb5012fd7080e5bb0b3ea
Daniel Chung <dan.chung.526@berkeley.edu>
2019-05-10 04:28:21 -0700

    Merge branch 'danstuff'

---
+++ AstroAlbedo/Assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairScript : MonoBehaviour {
+
+    public Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        mousePosition = mouseRay.origin + Vector3.Normalize(mouseRay.direction) * 30;
+        //mousePosition.z = 10f;
+        transform.position = mousePosition;
+        transform.LookAt(GameObject.Find("Player").transform, -Vector3.up);
+    }
+}
