cad93a639cbfef7b82e234b84ab1da5c989b404f
Min Jae Lee <minjl5@berkeley.edu>
2019-05-10 02:51:41 -0700

    diff camera

--- AstroAlbedo/Assets/Scripts/player.cs
+++ AstroAlbedo/Assets/Scripts/player.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
+
+	private const float X_ANGLE_MIN = -1f;
+	private const float X_ANGLE_MAX = 1f;
+	private const float Y_ANGLE_MIN = -10.0f;
+	private const float Y_ANGLE_MAX = 10.0f;
+
 	public float moveSpeed;
+	public float speed = 10;
 	public int hp;
 
 	// Use this for initialization
@@ -15,5 +22,11 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (moveSpeed * Input.GetAxis ("Alt Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis ("Alt Vertical") * Time.deltaTime);
+		float mouseInputX = Input.GetAxis ("Mouse X");
+		//float mouseInputY = Input.GetAxis ("Mouse Y");
+		//Debug.Log (mouseInput);
+		//mouseInputX = Mathf.Clamp (mouseInput, X_ANGLE_MIN, X_ANGLE_MAX);
+		Vector3 lookhere = new Vector3 (0, mouseInputX, 0);
+		transform.Rotate (lookhere);
 	}
 }
--- AstroAlbedo/Assets/Scripts/third_person_camera.cs
+++ AstroAlbedo/Assets/Scripts/third_person_camera.cs
@@ -4,36 +4,42 @@ using UnityEngine;
 
 public class third_person_camera : MonoBehaviour {
 
-	private const float Y_ANGLE_MIN = 0.0f;
-	private const float Y_ANGLE_MAX = 50.0f;
+	private const float X_ANGLE_MIN = -10.0f;
+	private const float X_ANGLE_MAX = 10.0f;
+	private const float Y_ANGLE_MIN = -10.0f;
+	private const float Y_ANGLE_MAX = 10.0f;
 
 	public Transform lookAt;
 	public Transform camTransform;
+	Vector3 offset = new Vector3 (0, 1.5f, -16f);
 
 	private Camera cam;
 
 	private float distance = 5.0f;
 	private float currentX = 0.0f;
 	private float currentY = 0.0f;
-	private float sensitivityX = 4.0f;
-	private float sensitivityY = 1.0f;
+	//private float sensitivityX = 4.0f;
+	//private float sensitivityY = 1.0f;
 
 	private void Start() {
 		camTransform = transform;
+		//camTransform.position = offset;
 		cam = Camera.main;
 	}
 
 	private void Update() {
 		currentX += Input.GetAxis ("Mouse X");
-		currentY -= Input.GetAxis ("Mouse Y");
+		//currentY -= Input.GetAxis ("Mouse Y");
 
-		currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+		currentX = Mathf.Clamp (currentX, X_ANGLE_MIN, X_ANGLE_MAX);
+		//currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
 	}
 
-	private void LateUpdate() {
+	/*private void LateUpdate() {
 		Vector3 dir = new Vector3 (0, 0, -distance);
-		Quaternion rotation = Quaternion.Euler (currentY, currentX, 0);
-		camTransform.position = lookAt.position + rotation * dir;
+		Quaternion rotation = Quaternion.Euler (0, currentX, 0);
+		Debug.Log (lookAt.position);
+		camTransform.position = offset + rotation * dir;
 		camTransform.LookAt (lookAt.position);
-	}
+	}*/
 }
